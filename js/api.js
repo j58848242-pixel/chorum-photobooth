e3d5a9802523d82e89d0e88d7761bc3362fd2411
j58848242-pixel <j58848242@gmail.com>
2026-09-03 05:49:09 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,16 +1,12 @@
 // ==========================================
-// KONFIGURASI JARINGAN CHORUM PHOTOBOOTH
+// KONFIGURASI JARINGAN CHORUM PHOTOBOOTH (SERVERLESS)
 // ==========================================
 
-// 1. Kunci API ImgBB (Mesin CDN Gambar & QR Code)
+// 1. Kunci API ImgBB (Mesin CDN Gambar)
 const IMGBB_API_KEY = "0a43872edad7bcf91d64198b42b5bdf3";
 
-// 2. Google Apps Script URL (HANYA untuk Cek Saklar Status Event ON/OFF)
-// Pastikan URL di bawah ini adalah hasil Deploy terbaru dari Code.gs Anda
-const API_URL = "https://script.google.com/macros/s/AKfycbzGA63-ANuO0ANIsIsdrbvBZ4b9toi4RVgtpl8_18lWO-qfKs3b2bs04KCcU09o3nrU/exec"; 
-
-// 3. Token Pengenal Event
-const EVENT_TOKEN = "CHORUM-001";
-
-// 4. URL Database Frame Lokal (Host di GitHub)
+// 2. URL Database Frame & Status Lokal (Host di GitHub)
 const FRAMES_JSON_URL = "assets/images/frames/frames.json";
+
+// 3. URL Web Utama Anda (Untuk Rute QR Code)
+const BASE_WEB_URL = "https://j58848242-pixel.github.io/chorum-photobooth/";
